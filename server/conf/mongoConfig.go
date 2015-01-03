57333aa310fc90ef34129744e304ad7e6e6525e6
Dmitry Papka <dmitry.papka@gmail.com>
2015-01-03 17:16:37 +0200

    MongoDB Configuration file added

---
+++ server/conf/mongoConfig.go
@@ -0,0 +1,16 @@
+package conf
+
+// MongoDB server host
+const MongoHost = "host-com"
+
+// MongoDB server port
+const MongoPort = "port-number"
+
+// MongoDB database name
+const MongoDatabase = "database-name"
+
+// MongoDB username for authorization
+const MongoUser = "mongo-user"
+
+// MongoDB password for authorization
+const MongoPassword = "mongo-password"
